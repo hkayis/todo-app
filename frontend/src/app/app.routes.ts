9341944cc8ba0cdafe531e9504ce3794f948d6b8
hkayis <h.kayis@hotmail.com>
2026-07-02 11:35:13 +0300

    guard eklendi

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,13 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { RegisterComponent } from './register/register';
 import { TodosComponent } from './todos/todos';
+import { authGuard } from './auth-guard';   
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'todos', component: TodosComponent },
-
+  { path: 'todos', component: TodosComponent, canActivate: [authGuard] },  
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-
   { path: '**', redirectTo: '/login' }
 ];
